0dad36a2b36b37a0ad812a47cdf9eb1f8f7d8700
Taras Shevchenko <tshevchenko@rightandabove.com>
2022-12-11 23:15:57 +0200

    added mui v.6

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import React, { FC, useCallback, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Moment from 'react-moment'
 import { AiOutlineMore } from 'react-icons/ai'
+import { styled } from '@mui/material'
 
 import { selectedBackgroundSelector, selectedLogoSelector, tokenSelector } from '../../store/selectors'
 import { UserApi } from '../../api'
@@ -9,9 +10,138 @@ import { Avatar } from '../Avatar'
 import { Loader } from '../Loader'
 import { LogoPicker } from '../LogoPicker'
 import { BackgroundPicker } from '../BackgroundPicker'
-
-import './Profile.css'
 import { IUser } from '../../store/types'
+
+const ProfileContainer = styled('div')(() => ({
+  position: 'relative',
+  margin: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: 20,
+  marginBottom: 20,
+  minWidth: 350,
+  minHeight: 400,
+  background: 'rgba(255, 255, 255, 0.1)',
+  boxShadow: '0 25px 45px rgba(0, 0, 0, 0.1)',
+  border: '1px solid rgba(255, 255, 255, 0.5)',
+  borderRight: '1px solid rgba(255, 255, 255, 0.2)',
+  borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
+  borderRadius: 10,
+  zIndex: 10,
+  backdropFilter: 'blur(25px)',
+}))
+
+const Settings = styled('form')<{ open: boolean }>(({ open }) => ({
+  display: 'none',
+  ...(open && {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    top: 20,
+    minWidth: 300,
+    minHeight: 420,
+    background: 'white',
+    boxShadow: '0 25px 45px rgba(0, 0, 0, 0.1)',
+    border: '1px solid rgba(255, 255, 255, 0.5)',
+    borderRight: '1px solid rgba(255, 255, 255, 0.2)',
+    borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
+    borderRadius: 5,
+    backdropFilter: 'blur(5px)',
+    zIndex: 99,
+  }),
+}))
+const SettingsTitle = styled('div')(() => ({
+  fontSize: 18,
+  fontWeight: 600,
+  margin: '10px auto',
+}))
+const SettingsAction = styled('div')(() => ({
+  display: 'flex',
+  margin: 10,
+}))
+const SettingsActionBtn = styled('div')(() => ({
+  fontSize: 18,
+  textAlign: 'center',
+  margin: 10,
+  padding: '5px 10px',
+  width: '100%',
+  maxWidth: 300,
+  background: 'rgba(255, 255, 255, 0.2)',
+  boxShadow: '0 25px 45px rgba(0, 0, 0, 0.1)',
+  border: '1px solid rgba(255, 255, 255, 0.5)',
+  borderRight: '1px solid rgba(255, 255, 255, 0.2)',
+  borderBottom: '1px solid rgba(255, 255, 255, 0.2)',
+  borderRadius: 5,
+  backdropFilter: 'blur(10px)',
+  cursor: 'pointer',
+  ['&:hover']: {
+    background: 'white',
+  },
+}))
+
+const Menu = styled('div')(() => ({
+  color: 'white',
+  position: 'absolute',
+  top: 20,
+  right: 20,
+  cursor: 'pointer',
+  ['&:hover']: {
+    color: '#5da6ff',
+  },
+}))
+
+const MenuOption = styled('div')<{ open: boolean }>(({ open }) => ({
+  display: 'none',
+  ...(open && {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    top: 20,
+    right: 50,
+    width: 150,
+    background: 'white',
+    borderRadius: 5,
+  }),
+  ['& hr']: {
+    width: '60%',
+    margin: 'auto',
+  },
+}))
+
+const MenuSelect = styled('button')(() => ({
+  width: '100%',
+  height: 40,
+  borderRadius: 5,
+  border: 'none',
+  fontSize: 18,
+  cursor: 'pointer',
+  ['&:hover']: {
+    color: '#5da6ff',
+  },
+}))
+
+const ProfileTitle = styled('h1')(() => ({
+  color: 'white',
+  textAlign: 'center',
+}))
+const ProfileContent = styled('div')(() => ({
+  margin: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  maxWidth: 350,
+}))
+const ProfileRow = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  width: '100%',
+  color: 'white',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '8px 16px',
+}))
 
 export const Profile: FC<{ user: IUser; reloadUser: () => void; reloadPosts: () => void }> = ({
   user,
@@ -47,35 +177,27 @@ export const Profile: FC<{ user: IUser; reloadUser: () => void; reloadPosts: () 
 
   if (!user) return <Loader />
   return (
-    <div className="prof-container">
-      <form className={`prof-setting-form${openForm ? ' open-form' : ''}`}>
-        <div className="prof-setting-title">Choose avatar</div>
+    <ProfileContainer>
+      <Settings open={openForm}>
+        <SettingsTitle>Choose avatar</SettingsTitle>
         <LogoPicker />
-        <div className="prof-setting-title">Choose background</div>
+        <SettingsTitle>Choose background</SettingsTitle>
         <BackgroundPicker />
-        <div className="prof-setting-actions">
-          <div className="prof-setting-btn" onClick={updateAvatar}>
-            Update avatar
-          </div>
-          <div className="prof-setting-btn" onClick={closeChangeAvatarModal}>
-            Close
-          </div>
-        </div>
-      </form>
+        <SettingsAction>
+          <SettingsActionBtn onClick={updateAvatar}>Update avatar</SettingsActionBtn>
+          <SettingsActionBtn onClick={closeChangeAvatarModal}>Close</SettingsActionBtn>
+        </SettingsAction>
+      </Settings>
       {token && (
-        <div className="prof-menu" onClick={openSettingAvatar}>
+        <Menu onClick={openSettingAvatar}>
           <AiOutlineMore size={'2em'} />
-        </div>
+        </Menu>
       )}
-      <div className={`prof-menu-option${open ? ' open' : ''}`}>
-        <button className="prof-menu-select" onClick={openChangeAvatarModal}>
-          Change avatar
-        </button>
+      <MenuOption open={open}>
+        <MenuSelect onClick={openChangeAvatarModal}>Change avatar</MenuSelect>
         <hr />
-        <button className="prof-menu-select" onClick={closeSettingAvatar}>
-          Cancel
-        </button>
-      </div>
+        <MenuSelect onClick={closeSettingAvatar}>Cancel</MenuSelect>
+      </MenuOption>
       <Avatar
         id={user.id}
         avatarLogo={user.avatarLogo}
@@ -83,29 +205,29 @@ export const Profile: FC<{ user: IUser; reloadUser: () => void; reloadPosts: () 
         username={user.username}
         isProfile={true}
       />
-      <h1 className="prof-title">{user.username}</h1>
-      <div className="prof-content">
-        <div className="prof-row">
+      <ProfileTitle>{user.username}</ProfileTitle>
+      <ProfileContent>
+        <ProfileRow>
           <div>Email: </div>
           <div>{user.email}</div>
-        </div>
-        <div className="prof-row">
+        </ProfileRow>
+        <ProfileRow>
           <div>Role: </div>
           <div>
             {user.roles.map((role) => (
               <div key={role.id}>{role.value}</div>
             ))}
           </div>
-        </div>
-        <div className="prof-row">
+        </ProfileRow>
+        <ProfileRow>
           <div>Posts: </div>
           <div>{user.posts.length}</div>
-        </div>
-        <div className="prof-row">
+        </ProfileRow>
+        <ProfileRow>
           <div>Register date: </div>
           <Moment date={user.createdAt} format="D MMM YYYY" />
-        </div>
-      </div>
-    </div>
+        </ProfileRow>
+      </ProfileContent>
+    </ProfileContainer>
   )
 }
